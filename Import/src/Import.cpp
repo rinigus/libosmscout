833edb8d5099bed6f5ab4578c2eada621d44ced3
Rinigus <rinigus.git@gmail.com>
2017-07-13 11:03:25 +0300

    sync with upstream

--- Import/src/Import.cpp
+++ Import/src/Import.cpp
@@ -384,10 +384,10 @@ int main(int argc, char* argv[])
   std::list<std::string>    mapfiles;
 
   osmscout::VehicleMask     defaultVehicleMask=osmscout::vehicleBicycle|osmscout::vehicleFoot|osmscout::vehicleCar;
-  bool                      deleteTemporaries=true;
-  bool                      deleteDebugging=true;
-  bool                      deleteAnalysis=true;
-  bool                      deleteReport=true;
+  bool                      deleteTemporaries=false;
+  bool                      deleteDebugging=false;
+  bool                      deleteAnalysis=false;
+  bool                      deleteReport=false;
 
   InitializeLocale(progress);
 
